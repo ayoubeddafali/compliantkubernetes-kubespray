ae155307111fd01cdbb9e57494738cfea663761c
Ayoub Ed-dafali <ayoub.eddafali@gmail.com>
2023-06-19 00:18:11 +0100

    updates

--- bin/common.bash
+++ bin/common.bash
@@ -380,13 +380,6 @@ ops_kubectl() { # <prefix> <args...>
     with_kubeconfig "$kubeconfig" kubectl "${@}"
 }
 
-containsElement () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
 assignHost() {
     local node=$1
     # Check for control plane nodes
@@ -416,4 +409,11 @@ assignHost() {
         fi
         addHostToGroup "${config[groups_inventory_file]}" "$node" "$target_group"
     fi
+}
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
 }
