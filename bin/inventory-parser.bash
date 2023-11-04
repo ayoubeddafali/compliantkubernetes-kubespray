c941aa2271fc3d26aa735a01ad39a91bbb568a10
Ayoub Ed-dafali <ayoubeddafali@gmail.com>
2023-11-04 20:59:12 +0100

    [config]: use ansible-inventory instead of gawk whenever possible

--- bin/inventory-parser.bash
+++ bin/inventory-parser.bash
@@ -6,117 +6,94 @@ here="$(dirname "$(readlink -f "$0")")"
 # shellcheck source=bin/common.bash
 source "${here}/common.bash"
 
+# Get inventory groups
+# Args: <inventory>
 read_inventory_groups(){
   local filename="$1"
-  gawk '{ if ($1 ~ /^\[[a-zA-Z0-9_:]{1,}\]/) section=tolower(gensub(/\[(.+)\]/,"\\1",1,$1)); configuration[section]=1 } END {for (key in configuration) { print key} }' "${filename}"
+  ansible-inventory -i "$1" --list | jq -r ". | keys[] | select(. != \"_meta\")"
 }
 
+# Get the hosts of a specific group
+# Args: <inventory> <group>
 get_group_hosts() {
   local filename="$1"
   local section="$2"
+  hosts=()
 
-  awk -v target_group="$section" \
-                -F' ' '{
-                      if ($1 ~ /^\[[a-zA-Z0-9_:]{1,}\]/)
-                        section=tolower(gensub(/\[(.+)\]/,"\\1",1,$1))
-                      else if ($1 !~ /^$/ && $1 !~ /^;/) {
-                        gsub(/^[ \t]+|[ \t]+$/, "", $1);
-                        gsub(/[\[\]]/, "", $1);
-                        if (section == target_group) {
-                          configuration[section][$1]=""
-                        }
-                      }
-                    }
-                    END {
-                        if ( length(configuration) > 0) {
-                          for (key in configuration[target_group]) { print key }
-                        } else {
-                          print ""
-                        }
-                    }' "${filename}"
+  if [[ ${section} == "all" ]]; then
+    readarray -td '' hosts < <(ansible-inventory -i "$filename" --list |  jq -r "._meta.hostvars | keys[]")
+  elif $(ansible-inventory -i "$filename" --list | jq ".$section | has(\"hosts\")" ); then
+    readarray -td '' hosts < <(ansible-inventory -i "$filename" --list | jq -r ".$section.hosts[]")
+  fi
+
+  echo "${hosts[@]}"
 }
 
+# Get the full section of a group
+# Args: <inventory> <group>
 get_section() {
   local filename="$1"
   local section="$2"
 
-  awk -v target_group="$section" \
-            -F' ' '{
-                    if ($1 ~ /^\[[a-zA-Z0-9_:]{1,}\]/) {
-                      section=tolower(gensub(/\[(.+)\]/,"\\1",1,$1))
-                      if (section == target_group) print $0
-                    }
-                    else if ($1 !~ /^$/ && $1 !~ /^;/) {
-                      if (section == target_group) {
-                        print $0
-                      }
-                    }
-                  }' "${filename}"
+  echo "[${section}]"
+  get_group_hosts "${filename}" "${section}"
 }
 
+# Get the value of a host variable
+# Args: <inventory> <host> <var>
 get_host_var() {
   local filename="$1"
   local host="$2"
   local hostvar="$3"
 
-  awk -v target_host="$host"  -v hostvar="$hostvar" \
-            -F' ' '{
-                      if ($1 ~ /^\[[a-zA-Z0-9_]{1,}\]/)
-                        section=tolower(gensub(/\[(.+)\]/,"\\1",1,$1))
-                      else if ($1 !~ /^$/ && $1 !~ /^;/) {
-                        gsub(/^[ \t]+|[ \t]+$/, "", $1);
-                        gsub(/[\[\]]/, "", $1);
-                        if (section == "all") {
-                          if ($1 == target_host ){
-                            for (i=2; i<=NF; i++){
-                              split($i, var, "=")
-                              if (var[1] == hostvar) {
-                                value = var[2]
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                    END {
-                        print value
-                    }' "${filename}"
+  ansible-inventory -i "${filename}" --list | jq -r "._meta.hostvars.${host}.${hostvar}"
 }
 
+# Check if host is part of a group
+# Args: <inventory> <host> <group>
+is_host_in_group() {
+  local filename="$1"
+  local host="$2"
+  local group="$3"
+
+  hosts="$(get_group_hosts "${filename}" "${group}")"
+  for h in ${hosts}; do
+    if [[ "$host" ==  "$h" ]]; then echo "true"; return; fi
+  done
+
+  echo "false";
+}
+
+# Check if a group exists
+# Args: <inventory> <group>
+group_exists() {
+  local filename="$1"
+  local group="$2"
+
+  groups="$(read_inventory_groups "$filename")"
+  for g in $groups; do
+    if [[ "$group" == "$g" ]]; then  echo "true"; return; fi
+  done
+
+  echo "false";
+}
+
+# Get a list of host variables
+# Args: <inventory> <host>
 get_host_vars() {
   local filename="$1"
   local host="$2"
-  if [[ "$(is_host_in_group "$filename" "$host" "all")" == "true" ]]; then
-    awk -v target_host="$host" \
-              -F' ' '{
-                        if ($1 ~ /^\[[a-zA-Z0-9_:]{1,}\]/)
-                          section=tolower(gensub(/\[(.+)\]/,"\\1",1,$1))
-                        else if ($1 !~ /^$/ && $1 !~ /^;/) {
-                          gsub(/^[ \t]+|[ \t]+$/, "", $1);
-                          gsub(/[\[\]]/, "", $1);
-                          if (section == "all") {
-                            if ($1 == target_host ){
-                              for (i=2; i<=NF; i++){
-                                split($i, var, "=")
-                                configuration[var[1]]=""
-                              }
-                            }
-                          }
-                        }
-                      }
-                      END {
-                        if ( length(configuration) > 0) {
-                            for (key in configuration) { print key }
-                          } else {
-                            print ""
-                          }
-                      }' "${filename}"
+  if [[ $(is_host_in_group "$filename" "$host" "all") == "true" ]]; then
+    ansible-inventory -i "${filename}" --list | jq -r "._meta.hostvars.${host} | keys[]"
   else
-    log_error "Host $host is not defined"
+    log_error "Host ${host} is not defined in the inventory"
     exit 1
   fi
 }
 
+# Set the value of a host variable, will add a new variable if doesn't exist
+# Args: <inventory> <host> <variable> <value>
+# NOTE: This only works for static inventories, not dynamic ones
 set_host_var() {
   local filename="$1"
   local host="$2"
@@ -160,6 +137,9 @@ set_host_var() {
 
 }
 
+# Remove a host variable
+# Args: <inventory> <host> <variable>
+# NOTE: This only works for static inventories, not dynamic ones
 unset_host_var() {
   local filename="$1"
   local host="$2"
@@ -197,6 +177,9 @@ unset_host_var() {
 
 }
 
+# Update the value of a host variable
+# Args: <inventory> <host> <variable> <newvalue>
+# NOTE: This only works for static inventories, not dynamic ones
 update_host_var() {
   local filename="$1"
   local host="$2"
@@ -235,28 +218,9 @@ update_host_var() {
 
 }
 
-is_host_in_group() {
-  local filename="$1"
-  local host="$2"
-  local group="$3"
-
-  hosts="$(get_group_hosts "${filename}" "${group}")"
-  for h in $hosts; do
-    if [[ "$host" ==  "$h" ]]; then echo "true"; break; fi
-  done
-}
-
-group_exists() {
-  local filename="$1"
-  local group="$2"
-
-  groups="$(read_inventory_groups "$filename")"
-  for g in $groups; do
-    if [[ "$group" == "$g" ]]; then  echo "true"; break; fi
-  done
-
-}
-
+# Add a new host to a group
+# Args: <inventory> <host> <group>
+# NOTE: This only works for static inventories, not dynamic ones
 add_host_to_group() {
   local filename="$1"
   local host="$2"
@@ -277,6 +241,9 @@ add_host_to_group() {
   fi
 }
 
+# Add to the end, a new host to a group
+# Args: <inventory> <host> <group>
+# NOTE: This only works for static inventories, not dynamic ones
 add_host_to_group_as_last() {
   local filename="$1"
   local host="$2"
@@ -298,12 +265,16 @@ add_host_to_group_as_last() {
   fi
 }
 
+
+# Remove a host from a group
+# Args: <inventory> <host> <group>
+# NOTE: This only works for static inventories, not dynamic ones
 remove_host_from_group() {
   local filename="$1"
   local host="$2"
   local group="$3"
 
-  if [[ "$(is_host_in_group "$filename" "$host" all)" == "true" ]]; then
+  if [[ "$(is_host_in_group "$filename" "$host" "$group")" == "true" ]]; then
     awk -v target_host="$host" -v target_group="$group" \
         -F' ' '{
                 global_removal=1
@@ -328,11 +299,14 @@ remove_host_from_group() {
     ' "${filename}" > /tmp/secondary-inventory.ini
     cp /tmp/secondary-inventory.ini "$filename"
   else
-    log_error "Host $host is not defined"
+    log_error "Could not remove host : $host, as it is not part of the group: $group"
     exit 1
   fi
 }
 
+# Add a new group to the inventory
+# Args: <inventory> <group>
+# NOTE: This only works for static inventories, not dynamic ones
 add_group() {
   local filename="$1"
   local group="$2"
